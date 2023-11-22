8b64084b73c403c08cedeaf614f4ab53b7303fde
Carlos Lapao <cjlapao@gmail.com>
2023-11-22 13:01:56 +0000

    fix the address

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "hashicorp.com/edu/parallels-desktop",
+		Address: "registry.terraform.io/parallels/parallels-desktop",
 		Debug:   debug,
 	}
 
